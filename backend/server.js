faf15d44c16d4a6d6167359940ba6a7c029d340d
phjemi <jeric.miana@e-horizon.com>
2023-09-15 22:52:14 +0800

    Fixed 'No Access-Control-Allow-Origin' header CORS

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,8 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
-
+var cors = require('cors');
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
